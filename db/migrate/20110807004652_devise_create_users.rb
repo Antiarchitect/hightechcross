63147b9a435055707f8d9c567892a303d10ccdbc
Andrey Voronkov <voronkovaa@gmail.com>
2011-08-07 11:09:02 +1000

    Small fix to users migration.

--- db/migrate/20110807004652_devise_create_users.rb
+++ db/migrate/20110807004652_devise_create_users.rb
@@ -12,18 +12,20 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.rememberable
       t.trackable
 
-      #t.recoverable
+      # t.recoverable
       # t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
+      # Regular attributes
+      t.string :name
 
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    # add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
