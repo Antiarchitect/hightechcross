9133814879fe457d18052029ddcd80b9d2b6a9d6
Andrey Voronkov <voronkovaa@gmail.com>
2011-08-08 14:13:05 +1000

    Fixed helper method to allow display more than 24 hours in results table.

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,5 +1,10 @@
 module ResultsHelper
   def format_time_distance(time)
-    (Time.mktime(0) + time).strftime('%H:%M')
+    # Approach in comment below inconsistent if general time is more than a day
+    #(Time.mktime(0) + time).strftime('%H:%M')
+    time = time.to_i
+    hours = (time / 3600)
+    minutes = (time % 3600) / 60
+    "#{hours}:#{minutes}"
   end
 end
